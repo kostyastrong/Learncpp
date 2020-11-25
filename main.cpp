34bad16df9054dcdc093ad16b574638e4f4e61d1
kostyastrong <kostyakostya872@gmail.com>
2020-11-25 21:06:07 +0300

    starting

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QLabel>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+//    QLabel *label = new QLabel("Hello");
+//    label->resize(400, 400);
+//    label->show();
+
     return a.exec();
-    int n = 5;
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,3 +13,8 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->AboveButton->setText("Оу, да");
+}
