c5ca1d5b61077c3647add2b8466748c03b389a9c
kostyastrong <kostyakostya872@gmail.com>
2020-11-25 21:50:50 +0300

    intro to blocks

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -16,5 +18,12 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    ui->AboveButton->setText("Оу, да");
+    QMessageBox::StandardButton b = QMessageBox::question(this, "Заголовок", "Text",
+                          QMessageBox::Yes | QMessageBox::No);
+    if (b == QMessageBox::Yes) {
+        QApplication::quit();
+    } else {
+        qDebug() << "No was yes";
+    }
+    //ui->AboveButton->setText("Оу, да");
 }
